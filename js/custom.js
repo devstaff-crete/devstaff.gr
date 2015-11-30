0cfcae3e3fd9b7fdb48c2764097a707cc23da462
DaKnOb <daknob.mac@gmail.com>
2015-11-30 18:48:55 +0200

    Revert "Make Next Meetup Easily Adjustable"
    This reverts commit 666c9e18dd145a157791f3acb3018ede755a4445.

--- js/custom.js
+++ js/custom.js
@@ -12,10 +12,3 @@ $(function() {
     }
   });
 });
-
-$("#NextLink").attr("href", NextLink);
-$("#NextMeetup").html(NextMeetup);
-$("#NextDate").html(NextDay + "<span>/" + NextMonth + "</span></h4><h3>" + NextYear + "</h3>");
-$("#NextMap").attr("href", NextMap);
-$("#NextLocation").html("<h5>" + NextLocationTitle + "</h5><h6>" + NextLocationSubtitle + "</h6>");
-$("#NextTime").html(NextTime);
--- ticker.js
+++
@@ -1,9 +0,0 @@
-var NextMeetup = "Unit <br><span>Testing</span>";
-var NextDay = "10";
-var NextMonth = "12";
-var NextYear = "2015";
-var NextLocationTitle = "Main Building";
-var NextLocationSubtitle = "FoRTH";
-var NextTime = "20:00";
-var NextLink = "http://www.meetup.com/DevStaff-A-Developer-Community-Gathering-In-Crete/events/226695959/";
-var NextMap = "https://www.google.gr/maps/place/Foundation+for+Research+and+Technology+-+Hellas/@35.3051351,25.0725918,17z/data=!4m2!3m1!1s0x0000000000000000:0x0c8347768b841f2f?hl=en";
