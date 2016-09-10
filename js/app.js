d60dc3099f9f70c34074bb5d928e0f8a3a91436f
Thanassis Zografos <tzografos@gmail.com>
2016-09-10 18:07:42 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,6 +6,30 @@ angular.module('PastMeetupsApp.controllers', []).
 controller('PastMeetupsController', function($scope) {
 	$scope.MeetupList = [
                         {
+                        Name: "???",
+                        Day: 13,
+                        Month: 10,
+                        Year: 2016,
+                        Hour: "20:00",
+                        Location: "Main Building",
+                        Location2: "FoRTH",
+                        GitHubLink: "#",
+                        LocationLink: "https://www.google.gr/maps/place/Foundation+for+Research+and+Technology+-+Hellas/@35.3051351,25.0725918,17z/data=!4m2!3m1!1s0x0000000000000000:0x0c8347768b841f2f?hl=en",
+                        MeetupLink: "#",
+                        FacebookLink: "https://www.facebook.com/Devstaff/"
+                },{
+                        Name: "Lightning talks",
+                        Day: 8,
+                        Month: 9,
+                        Year: 2016,
+                        Hour: "20:00",
+                        Location: "Main Building",
+                        Location2: "FoRTH",
+                        GitHubLink: "https://github.com/devstaff-crete/meetup13-LightningTalks-SoftwareballGame",
+                        LocationLink: "https://www.google.gr/maps/place/Foundation+for+Research+and+Technology+-+Hellas/@35.3051351,25.0725918,17z/data=!4m2!3m1!1s0x0000000000000000:0x0c8347768b841f2f?hl=en",
+                        MeetupLink: "http://www.meetup.com/DevStaff-A-Developer-Community-Gathering-In-Crete/events/233882249/",
+                        FacebookLink: "https://www.facebook.com/Devstaff/"
+                },{
                         Name: "Password Management",
                         Day: 14,
                         Month: 7,
