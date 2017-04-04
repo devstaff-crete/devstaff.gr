5a81347070de8772fa2c6dd37d1d9877f0e950cd
Thanassis Zografos <tzografos@ktapult-group.com>
2017-04-04 11:51:06 +0300

    meetup 21 added

--- js/app.js
+++ js/app.js
@@ -5,6 +5,19 @@ angular.module('PastMeetupsApp', [
 angular.module('PastMeetupsApp.controllers', []).
 controller('PastMeetupsController', function($scope) {
     $scope.MeetupList = [{
+            Name: "Remote Working",
+            Day: 06,
+            Month: 04,
+            Year: 2017,
+            Hour: "20:00",
+            Location: "Main Building",
+            Location2: "FoRTH",
+            GitHubLink: "https://github.com/devstaff-crete/meetup21-RemoteWorking",
+            LocationLink: "https://www.google.gr/maps/place/Foundation+for+Research+and+Technology+-+Hellas/@35.3051351,25.0725918,17z/data=!4m2!3m1!1s0x0000000000000000:0x0c8347768b841f2f?hl=en",
+            MeetupLink: "https://www.meetup.com/DevStaff-A-Developer-Community-Gathering-In-Crete/events/238459952/",
+            FacebookLink: "https://www.facebook.com/Devstaff/"
+        },
+        {
             Name: "TOR",
             Day: 09,
             Month: 03,
