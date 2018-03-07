831c3cfc48838114b0ec964c09faa3cf6636e8e9
sonaht <tzografos@gmail.com>
2018-03-07 15:46:50 +0200

    meetup32 added

--- js/app.js
+++ js/app.js
@@ -5,6 +5,18 @@ angular.module('PastMeetupsApp', [
 angular.module('PastMeetupsApp.controllers', []).
 controller('PastMeetupsController', function($scope) {
     $scope.MeetupList = [{
+            Name: "From Coder to Manager",
+            Day: 08,
+            Month: 02,
+            Year: 2018,
+            Hour: "20:00",
+            Location: "Main Building",
+            Location2: "FoRTH",
+            GitHubLink: "https://github.com/devstaff-crete/meetup32-FromCoderToManager",
+            LocationLink: "https://www.google.gr/maps/place/Foundation+for+Research+and+Technology+-+Hellas/@35.3051351,25.0725918,17z/data=!4m2!3m1!1s0x0000000000000000:0x0c8347768b841f2f?hl=en",
+            MeetupLink: "https://www.meetup.com/devstaff/events/248331146/",
+            FacebookLink: "https://www.facebook.com/events/2267792883246446/"
+        }, {
             Name: "Functional Programming",
             Day: 15,
             Month: 02,
