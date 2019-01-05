626729ad0e9a9bb1ef302e2a1edad63cf101fbca
Michalis Zampetakis <mzampetakis@gmail.com>
2019-01-05 15:47:41 +0200

    Added meetup 41

--- js/app.js
+++ js/app.js
@@ -49,6 +49,19 @@ angular.module('PastMeetupsApp', [
 angular.module('PastMeetupsApp.controllers', []).
 controller('PastMeetupsController', function($scope) {
     $scope.MeetupList = [{
+            Name: "Disaster Recovery: Beyond Backup Strategies",
+            Day: 10,
+            Month: 01,
+            Year: 2019,
+            Hour: "19:00",
+            Location: "Leoforos Ikarou 5 · Heraklion",
+            Location2: "Pancretan Cooperative Bank",
+            GitHubLink: "https://github.com/devstaff-crete/meetup41-DisasterRecovery",
+            LocationLink: "https://www.google.com/maps/search/?api=1&query=35.338970%2C25.140550",
+            MeetupLink: "https://www.meetup.com/devstaff/events/257759964/",
+            FacebookLink: "https://www.facebook.com/events/232216687657699/",
+            IsFutureMeetup: 1
+        },{
             Name: "Elasticsearch",
             Day: 13,
             Month: 12,
