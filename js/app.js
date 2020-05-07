b5a360bc02ea0ea126ff57863bc066e26c7beb14
mzampetakis <mzampetakis@gmail.com>
2020-05-07 22:14:27 +0300

    Fix typo

--- js/app.js
+++ js/app.js
@@ -88,12 +88,12 @@ controller('PastMeetupsController', function($scope) {
   };
   $scope.MeetupList = [
     {
-      Name: "Lighting Talks",
+      Name: "Lightning Talks",
       Date: "14/05/2020",
       Hour: "19:00",
       Location: "Online meetup",
       Location2: "through zoom",
-      GitHubLink: "https://github.com/devstaff-crete/DevStaff-Heraklion/tree/master/meetups/meetup54-LightingTalks",
+      GitHubLink: "https://github.com/devstaff-crete/DevStaff-Heraklion/tree/master/meetups/meetup54-LightningTalks",
       LocationLink: "",
       MeetupLink: "https://www.meetup.com/devstaff/events/270468862/"
     },
